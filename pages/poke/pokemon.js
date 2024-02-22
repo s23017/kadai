e6013e61897e5f76ef177def2d0f2bf06b736843
s23017 <s23017@std.it-college.ac.jp>
2024-02-22 05:26:56 +0000

    japanes pokemon name
    ポケモンの名前を日本語化

--- pages/poke/pokemon.js
+++ pages/poke/pokemon.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+const pokemon = require('pokemon')
 
 const Pokemon = () => {
   const [pokemonData, setPokemonData] = useState(null)
@@ -6,7 +7,7 @@ const Pokemon = () => {
   useEffect(() => {
     const fetchPokemonData = async () => {
       try {
-        const randomPokemonId = Math.floor(Math.random() * 1025) + 1
+        const randomPokemonId = Math.floor(Math.random() * 1024) + 1
         const response = await fetch(
           `https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`
         )
@@ -28,8 +29,11 @@ const Pokemon = () => {
       <h1>Pokemon</h1>
       {pokemonData && (
         <div>
-          <p>Name: {pokemonData.name}</p>
+          <p>Name: {pokemon.getName(pokemonData.id, 'ja')}</p>
           <p>ID: {pokemonData.id}</p>
+          <p>
+            Types: {pokemonData.types.map(type => type.type.name).join(', ')}
+          </p>
         </div>
       )}
     </div>
